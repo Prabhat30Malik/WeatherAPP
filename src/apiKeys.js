df42beccdcc9d389fe43969d104fa1bbc6167ad4
Bhavesh2091 <mapsbhavesh2091@gmail.com>
2024-03-24 16:07:07 +0530

    first

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "9f7b40bf2a7e605a24d737ddd48058f9",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
